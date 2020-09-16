e37ff72cef66aae208dc663e47c3aea247c1b314
matiasguerrero <maguerrero@alumnos.exa.unicen.edu.ar>
2020-09-16 17:57:35 -0300

    Upd FileProcessor y TablaSimbolos

--- src/analizador_lexico/TablaDeSimbolos.java
+++ src/analizador_lexico/TablaDeSimbolos.java
@@ -1,38 +1,45 @@
 package analizador_lexico;
 
 import java.util.Hashtable;
+import java.util.Map;
 
 public class TablaDeSimbolos {
-    private Hashtable<Integer, Celda> tablaSimb;
+    private Hashtable<String, Celda> tablaSimb;
 
     public TablaDeSimbolos(){
         tablaSimb=new Hashtable<>();
     }
 
     /**
-     * Agrega una celda (token,lexema,tipo). En caso de existir previamente, retorna el objeto. Sino, null.
+     * Agrega una celda (token,lexema,tipo). En caso de existir previamente, retorna el value anterior.
      * @param celda
      * @return
      */
     public Celda agregar(Celda celda){
-        if (tablaSimb.containsKey(celda.getToken()))
-            return tablaSimb.get(celda.getToken());
-        else {
-            tablaSimb.put(celda.getToken(), celda);
-            return null;
-        }
+        if (!tablaSimb.containsKey(celda.getLexema()))
+            tablaSimb.put(celda.getLexema(), celda);
+       return getValor(celda.getLexema());
     }
 
     /**
      * Elimina una celda dado el token
-     * @param token
+     * @param lexema
      * @return
      */
-    public boolean eliminar(int token){
-        if (tablaSimb.containsKey(token)){
-            tablaSimb.remove(token);
+    public boolean eliminar(String lexema){
+        if (tablaSimb.containsKey(lexema)){
+            tablaSimb.remove(lexema);
             return true;
         }
         return false;
+    }
+
+    /**
+     * Dado un lexema, devuelve la celda en la tabla de simbolos.
+     * @param lexema
+     * @return Celda o null en caso de no existir.
+     */
+    public Celda getValor(String lexema){
+        return tablaSimb.get(lexema);
     }
 }
